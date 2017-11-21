512f4dd24e7de35e0802973e32ff136322808371
roccia <lzzy.ywz@gmail.com>
2017-11-21 15:09:27 +0800

    fix

--- app/models/twitter_info.rb
+++ app/models/twitter_info.rb
@@ -2,14 +2,14 @@ class TwitterInfo < ApplicationRecord
 
   def self.get_user_info(username)
     client = TwitterApi::GetInfo.get_client
-    tweets_info = client.home_timeline
+    tweets_info = client.user_timeline(username)
     tweets_info.first(10).map do |t|
       content = t.text
       created_at = t.created_at
      {tweets: content, created: created_at}
     end
 
-   end
+  end
 
 
 end
